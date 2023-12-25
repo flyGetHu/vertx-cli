bc6c1eea5334db0957a4c52f635da6cb028d31d3
huan <970780868@qq.com>
2023-12-25 18:52:35 +0800

    webservice

--- common-webserver/src/main/java/webserver/helper/VertxWebHelper.java
+++ common-webserver/src/main/java/webserver/helper/VertxWebHelper.java
@@ -15,28 +15,44 @@ import static com.vertx.common.core.entity.web.ApiResponse.errorResponse;
 
 public class VertxWebHelper {
     public void startHttpServer(WebServiceOptions options) {
+        // 判断是否已经初始化
         if (!isInit) {
             StaticLog.error("VertxLoadConfig is not init");
             throw new webserver.exception.WebServerStartException("VertxLoadConfig is not init");
         }
+        // 创建HTTP服务器选项
         final HttpServerOptions httpServerOptions = new HttpServerOptions();
+        // 获取应用程序配置中的Web服务器选项
         final AppConfig.WebServer configWebServer = appConfig.getWebServer();
+        // 如果webServer为空，则抛出异常
         if (configWebServer == null) {
             StaticLog.error("webServer is null");
             throw new webserver.exception.WebServerStartException("webServer is null");
         }
+        // 设置服务器的主机和端口
         httpServerOptions.setHost(configWebServer.getHost());
         httpServerOptions.setPort(configWebServer.getPort());
+        // 设置服务器的超时时间
         httpServerOptions.setIdleTimeout(configWebServer.getTimeout());
+        // 设置服务器的ALPN版本
         httpServerOptions.setAlpnVersions(configWebServer.getAlpnVersions());
+        // 设置注册WebSocket写处理程序
         httpServerOptions.setRegisterWebSocketWriteHandlers(true);
+        // 设置是否支持压缩
         httpServerOptions.setCompressionSupported(configWebServer.isCompressionSupported());
+        // 设置压缩级别
         httpServerOptions.setCompressionLevel(configWebServer.getCompressionLevel());
+        // 创建HTTP服务器
         final HttpServer httpServer = vertx.createHttpServer(httpServerOptions);
+        // 创建路由器
         final Router mainRouter = Router.router(vertx);
+        // 创建日志处理器
         LoggerHandler loggerHandler = LoggerHandler.create(LoggerFormat.CUSTOM);
+        // 设置自定义的日志格式
         loggerHandler.customFormatter(new CustomerLog());
+        // 配置跨域处理
         mainRouter.route("/*").handler(io.vertx.ext.web.handler.CorsHandler.create())
+                // 配置日志处理中间件
                 .handler(context -> {
                     if (configWebServer.isLogEnabled()) {
                         loggerHandler.handle(context);
@@ -44,33 +60,33 @@ public class VertxWebHelper {
                         context.next();
                     }
                 });
+        // 创建另一个路由器
         final Router router = Router.router(vertx);
+        // 初始化路由器
         options.initRouter.init(router);
+        // 将路由器添加到主路由器
         mainRouter.route(configWebServer.getPrefix()).subRouter(router);
-
         // 超时异常处理
         mainRouter.errorHandler(HttpStatus.HTTP_UNAVAILABLE, context -> {
             StaticLog.error(context.failure(), "接口超时:{}", context.request().path());
             context.end(errorResponse(HttpStatus.HTTP_INTERNAL_ERROR, "接口超时,请联系管理员!"));
         });
-
         // 404异常处理
         mainRouter.errorHandler(HttpStatus.HTTP_NOT_FOUND, context -> {
             StaticLog.error(context.failure(), "接口不存在:{}", context.request().path());
             context.end(errorResponse(HttpStatus.HTTP_NOT_FOUND, "接口不存在,请联系管理员!"));
         });
-
         // 405异常处理
         mainRouter.errorHandler(HttpStatus.HTTP_BAD_METHOD, context -> {
             StaticLog.error(context.failure(), "接口不支持:{}", context.request().path());
             context.end(errorResponse(HttpStatus.HTTP_BAD_METHOD, "接口不支持,请联系管理员!"));
         });
-
         // 统一处理异常
         mainRouter.errorHandler(HttpStatus.HTTP_INTERNAL_ERROR, context -> {
             StaticLog.error(context.failure(), "接口异常:{}", context.request().path());
             context.end(errorResponse(HttpStatus.HTTP_INTERNAL_ERROR, "接口异常,请联系管理员!"));
         });
+        // 监听HTTP服务器
         httpServer.requestHandler(mainRouter).listen(configWebServer.getPort()).onComplete(res -> {
             if (res.succeeded()) {
                 StaticLog.info("start http server:{}:{}", configWebServer.getHost(), configWebServer.getPort());
--- example-web/src/main/java/com/vertx/example/web/verticle/WebVerticle.java
+++ example-web/src/main/java/com/vertx/example/web/verticle/WebVerticle.java
@@ -7,10 +7,20 @@ import webserver.helper.VertxWebHelper;
 
 public class WebVerticle extends AbstractVerticle {
 
+    /**
+     * 启动方法
+     * @throws Exception 异常
+     */
     @Override
     public void start() throws Exception {
+        // 创建WebServiceOptions对象
         final WebServiceOptions webServiceOptions = new WebServiceOptions();
+
+        // 初始化WebRouter
         webServiceOptions.initRouter = new WebRouterInit();
+
+        // 启动HTTP服务器
         new VertxWebHelper().startHttpServer(webServiceOptions);
     }
+
 }

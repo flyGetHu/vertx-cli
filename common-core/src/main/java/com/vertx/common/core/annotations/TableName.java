dc13280581db4a6f74c0fd8e6a0d8c9021848f19
huan <970780868@qq.com>
2024-01-04 10:55:53 +0800

    init

--- common-core/src/main/java/com/vertx/common/core/annotations/TableName.java
+++ common-core/src/main/java/com/vertx/common/core/annotations/TableName.java
@@ -3,8 +3,12 @@ package com.vertx.common.core.annotations;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
+/**
+ * 数据库表名注解
+ */
 @Target({java.lang.annotation.ElementType.TYPE})
 @Retention(java.lang.annotation.RetentionPolicy.RUNTIME)
 public @interface TableName {
+    // 表名
     String name() default "";
 }
--- common-core/src/main/java/com/vertx/common/core/annotations/UniqueAddress.java
+++ common-core/src/main/java/com/vertx/common/core/annotations/UniqueAddress.java
@@ -4,8 +4,12 @@ package com.vertx.common.core.annotations;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
+/**
+ * 唯一地址
+ */
 @Target({java.lang.annotation.ElementType.TYPE})
 @Retention(java.lang.annotation.RetentionPolicy.RUNTIME)
 public @interface UniqueAddress {
+    // 唯一地址
     String value() default "";
 }

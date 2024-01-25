8de662412b61a018dfc1b94562b501cc00a58ba8
huan <970780868@qq.com>
2024-01-25 22:16:17 +0800

    Add LocalLockUtil class and RandUtil class
    Add LocalLockUtilTest class

--- common-core/src/main/java/com/vertx/common/core/utils/RandUtil.java
+++ common-core/src/main/java/com/vertx/common/core/utils/RandUtil.java
@@ -5,7 +5,18 @@ import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.util.Base64;
 
+/**
+ * Utility class for generating random values.
+ */
 public class RandUtil {
+  /**
+   * 使用SHA-256算法对给定的密码进行哈希处理，并返回盐和哈希密码，
+   * 编码为Base64字符串。
+   *
+   * @param password 需要进行哈希处理的密码
+   * @return 盐和哈希密码编码为Base64字符串
+   * @throws NoSuchAlgorithmException 如果SHA-256算法不可用
+   */
   public static String hashPassword(String password) throws NoSuchAlgorithmException {
     SecureRandom random = new SecureRandom();
     byte[] salt = new byte[16];
@@ -19,6 +30,14 @@ public class RandUtil {
     return Base64.getEncoder().encodeToString(salt) + "$" + Base64.getEncoder().encodeToString(hashedPassword);
   }
 
+  /**
+   * 验证给定的密码是否与带盐的哈希密码匹配。
+   *
+   * @param password               需要验证的密码。
+   * @param hashedPasswordWithSalt 带盐的哈希密码。
+   * @return 如果密码与带盐的哈希密码匹配，则返回true，否则返回false。
+   * @throws NoSuchAlgorithmException 如果SHA-256算法不可用。
+   */
   public static boolean verifyPassword(String password, String hashedPasswordWithSalt) throws NoSuchAlgorithmException {
     String[] parts = hashedPasswordWithSalt.split("\\$");
     byte[] salt = Base64.getDecoder().decode(parts[0]);

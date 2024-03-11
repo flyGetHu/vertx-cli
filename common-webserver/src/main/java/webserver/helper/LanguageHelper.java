e47c329c66a8757727e643824880375568e6b2e6
huan <970780868@qq.com>
2024-03-11 11:40:27 +0800

    Refactor QueryPageParam and add LockUtilTest

--- common-webserver/src/main/java/webserver/helper/LanguageHelper.java
+++ common-webserver/src/main/java/webserver/helper/LanguageHelper.java
@@ -29,111 +29,112 @@ import static io.vertx.core.Future.await;
  */
 public class LanguageHelper {
 
-    /**
-     * 语言数据的静态数据
-     */
-    private static final Map<String, LanguageData> languageDataMap = new java.util.HashMap<>();
+  /**
+   * 语言数据的静态数据
+   */
+  private static final Map<String, LanguageData> languageDataMap = new java.util.HashMap<>();
 
-    /**
-     * 根据提供的名称和语言类型检索特定于语言的字符串。
-     *
-     * @param name             字符串的名称。
-     * @param languageTypeEnum 表示语言类型的枚举。
-     * @param args             用于字符串格式化的可选参数。
-     * @return 特定于语言的字符串，如果未找到语言数据，则返回名称本身。
-     */
-    public static String getLanguageString(String name, LanguageTypeEnum languageTypeEnum, String... args) {
-        if (languageDataMap.isEmpty()) {
-            if (!loadConfigFile()) return name;
-        }
-        if (languageDataMap.isEmpty()) {
-            StaticLog.warn("language.json 文件为空,无法获取语言信息");
-            return name;
-        }
-        LanguageData languageData = languageDataMap.get(name);
-        if (languageData == null) {
-            StaticLog.warn("language.json 文件中不存在 name 为" + name + "的语言信息,请补充");
-            return name;
-        }
-        String msg = switch (languageTypeEnum.name().toLowerCase()) {
-            case "zh" -> languageData.getZh();
-            case "pu" -> languageData.getPu();
-            default -> languageData.getEn();
-        };
-        return StrUtil.format(msg, args);
+  /**
+   * 根据提供的名称和语言类型检索特定于语言的字符串。
+   *
+   * @param name             字符串的名称。
+   * @param languageTypeEnum 表示语言类型的枚举。
+   * @param args             用于字符串格式化的可选参数。
+   * @return 特定于语言的字符串，如果未找到语言数据，则返回名称本身。
+   */
+  public static String getLanguageString(String name, LanguageTypeEnum languageTypeEnum, String... args) {
+    if (languageDataMap.isEmpty()) {
+      if (!loadConfigFile())
+        return name;
     }
+    if (languageDataMap.isEmpty()) {
+      StaticLog.warn("language.json 文件为空,无法获取语言信息");
+      return name;
+    }
+    LanguageData languageData = languageDataMap.get(name);
+    if (languageData == null) {
+      StaticLog.warn("language.json 文件中不存在 name 为" + name + "的语言信息,请补充");
+      return name;
+    }
+    String msg = switch (languageTypeEnum.name().toLowerCase()) {
+      case "zh" -> languageData.getZh();
+      case "pu" -> languageData.getPu();
+      default -> languageData.getEn();
+    };
+    return StrUtil.format(msg, (Object[]) args);
+  }
 
-    /**
-     * 加载语言配置文件
-     *
-     * @return 是否加载成功
-     */
-    private static boolean loadConfigFile() {
-        final String filePath = "i18/language.json";
-        // 添加锁 防止多次读取文件
-        final Lock localLock = SharedLockHelper.getLocalLock(SharedLockSharedLockEnum.INIT_LANGUAGE, null);
-        try {
-            if (!languageDataMap.isEmpty()) {
-                return true;
-            }
-            AsyncFile asyncFile = null;
-            try {
-                if (!await(vertx.fileSystem().exists(filePath))) {
-                    StaticLog.warn("language.json文件不存在,无法获取语言信息");
-                    return false;
-                }
-                asyncFile = await(vertx.fileSystem().open(filePath, new OpenOptions().setRead(true)));
-                final Long fileSize = await(asyncFile.size());
-                final Buffer buffer = await(asyncFile.read(Buffer.buffer(), 0, 0, Math.toIntExact(fileSize)));
-                final JsonArray languageDataList = buffer.toJsonArray();
-                final Map<String, Integer> languageDataMapUnique = new java.util.HashMap<>();
-                for (Object item : languageDataList) {
-                    final LanguageData languageData = Json.decodeValue(item.toString(), LanguageData.class);
-                    final String key = languageData.getName();
-                    if (!languageDataMapUnique.containsKey(key)) {
-                        languageDataMapUnique.put(key, 1);
-                    } else {
-                        StaticLog.warn("language.json文件中存在重复的name为{}的语言信息,请检查");
-                    }
-                    languageDataMap.put(key, languageData);
-                }
-            } finally {
-                if (asyncFile != null) {
-                    await(asyncFile.close());
-                }
-            }
-        } finally {
-            localLock.release();
-        }
+  /**
+   * 加载语言配置文件
+   *
+   * @return 是否加载成功
+   */
+  private static boolean loadConfigFile() {
+    final String filePath = "i18/language.json";
+    // 添加锁 防止多次读取文件
+    final Lock localLock = SharedLockHelper.getLocalLock(SharedLockSharedLockEnum.INIT_LANGUAGE, null);
+    try {
+      if (!languageDataMap.isEmpty()) {
         return true;
+      }
+      AsyncFile asyncFile = null;
+      try {
+        if (!await(vertx.fileSystem().exists(filePath))) {
+          StaticLog.warn("language.json文件不存在,无法获取语言信息");
+          return false;
+        }
+        asyncFile = await(vertx.fileSystem().open(filePath, new OpenOptions().setRead(true)));
+        final Long fileSize = await(asyncFile.size());
+        final Buffer buffer = await(asyncFile.read(Buffer.buffer(), 0, 0, Math.toIntExact(fileSize)));
+        final JsonArray languageDataList = buffer.toJsonArray();
+        final Map<String, Integer> languageDataMapUnique = new java.util.HashMap<>();
+        for (Object item : languageDataList) {
+          final LanguageData languageData = Json.decodeValue(item.toString(), LanguageData.class);
+          final String key = languageData.getName();
+          if (!languageDataMapUnique.containsKey(key)) {
+            languageDataMapUnique.put(key, 1);
+          } else {
+            StaticLog.warn("language.json文件中存在重复的name为{}的语言信息,请检查");
+          }
+          languageDataMap.put(key, languageData);
+        }
+      } finally {
+        if (asyncFile != null) {
+          await(asyncFile.close());
+        }
+      }
+    } finally {
+      localLock.release();
     }
+    return true;
+  }
 
-    /**
-     * 根据提供的名称和 RoutingContext 的“Accept-Language”标头检索特定于语言的字符串。
-     *
-     * @param name    字符串的名称。
-     * @param context 表示传入 HTTP 请求的 RoutingContext 对象。
-     * @param args    用于字符串格式化的可选参数。
-     * @return 特定于语言的字符串，如果未找到语言数据，则返回名称本身。
-     */
-    public static String getLanguageString(RoutingContext context, String name, String... args) {
-        return getLanguageString(name, getLanguageType(context), args);
-    }
+  /**
+   * 根据提供的名称和 RoutingContext 的“Accept-Language”标头检索特定于语言的字符串。
+   *
+   * @param name    字符串的名称。
+   * @param context 表示传入 HTTP 请求的 RoutingContext 对象。
+   * @param args    用于字符串格式化的可选参数。
+   * @return 特定于语言的字符串，如果未找到语言数据，则返回名称本身。
+   */
+  public static String getLanguageString(RoutingContext context, String name, String... args) {
+    return getLanguageString(name, getLanguageType(context), args);
+  }
 
-    /**
-     * 根据提供的 RoutingContext 的“Accept-Language”标头检索语言类型。
-     *
-     * @param context 表示传入 HTTP 请求的 RoutingContext 对象。
-     * @return 与“Accept-Language”标头值对应的 LanguageTypeEnum。
-     * 如果标头不存在或与任何支持的语言类型不匹配，则默认值为 LanguageTypeEnum.ZH。
-     */
-    public static LanguageTypeEnum getLanguageType(RoutingContext context) {
-        final String languageHeader = context.request().getHeader("Accept-Language");
-        String header = StrUtil.isBlank(languageHeader) ? LanguageTypeEnum.ZH.name() : languageHeader;
-        return switch (header) {
-            case "en" -> LanguageTypeEnum.EN;
-            case "pu" -> LanguageTypeEnum.PU;
-            default -> LanguageTypeEnum.ZH;
-        };
-    }
+  /**
+   * 根据提供的 RoutingContext 的“Accept-Language”标头检索语言类型。
+   *
+   * @param context 表示传入 HTTP 请求的 RoutingContext 对象。
+   * @return 与“Accept-Language”标头值对应的 LanguageTypeEnum。
+   *         如果标头不存在或与任何支持的语言类型不匹配，则默认值为 LanguageTypeEnum.ZH。
+   */
+  public static LanguageTypeEnum getLanguageType(RoutingContext context) {
+    final String languageHeader = context.request().getHeader("Accept-Language");
+    String header = StrUtil.isBlank(languageHeader) ? LanguageTypeEnum.ZH.name() : languageHeader;
+    return switch (header) {
+      case "en" -> LanguageTypeEnum.EN;
+      case "pu" -> LanguageTypeEnum.PU;
+      default -> LanguageTypeEnum.ZH;
+    };
+  }
 }
